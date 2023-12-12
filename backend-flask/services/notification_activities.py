ed433f41afa1f8519cdbaf154c50a0e4c1091e6b
Francisco Nieto <vivertido@gmail.com>
2023-12-12 19:28:27 +0000

    added frontend notifications page

--- backend-flask/services/notification_activities.py
+++ backend-flask/services/notification_activities.py
@@ -14,8 +14,8 @@ class NotificationsActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Paco',
-        'message': 'Muy bien',
+        'handle':  'Papi',
+        'message': 'Muy bien, bebe',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
