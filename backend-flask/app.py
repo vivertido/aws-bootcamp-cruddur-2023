8dc082172cff7e16a2db857d8b41b8037f56902b
Francisco Nieto <vivertido@gmail.com>
2023-12-15 19:49:54 +0000

    added more honeycomb inits to app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,7 +15,29 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#honeycomb
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry import trace
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+#Initialize Honeycomb instruments
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+#Init app
 app = Flask(__name__)
+
+#Honeycomb---
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
